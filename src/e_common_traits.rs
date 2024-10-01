ee0be9446a0fabd1cc537f7f58991bb6b76ba855
Rama Baliola <rama@mandalachain.io>
2024-10-01 12:52:02 +0800

    first commit

---
+++ src/e_common_traits.rs
@@ -0,0 +1,183 @@
+//! This portion will test your familiarity with some of Rust's common traits and your ability to
+//! implement those traits in interesting ways. You need to remove all of the `todo!()`s. Most of
+//! them will need to be replaced by some code, but some may simply be deleted.
+
+// NOTE: You will need to `use` something from the standard library to implement `Ord` and
+// `PartialOrd` here.
+
+use std::cmp::Ordering;
+
+/// A record of an employee at a particular company
+#[derive(Debug)]
+pub struct Employee {
+	/// The name the person likes to be called. Doesn't have to be their "passport name"
+	pub name: String,
+	/// Amount of experience (in months) the person has working at this company
+	pub experience: u32,
+	/// Hourly wage paid to this employee
+	pub wage: u32,
+	/// Unique identifier for this employee
+	pub uid: u32,
+}
+
+// We want to consider two employee instances equal iff they have the same `uid`.
+
+impl PartialEq for Employee {
+	fn eq(&self, other: &Self) -> bool {
+		self.uid == other.uid
+	}
+}
+impl Eq for Employee {}
+
+// We want to sort employees. First and foremost, employees are equal if they have the same
+// `uid`, as explained above. For employees who are not equal, we sort by the value they
+// bring to the company. Value is defined as the quotient of the experience they've acquired
+// at the company divided by their wage. Use integer division for the purpose of this calculation.
+
+impl PartialOrd for Employee {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		match self.uid.partial_cmp(&other.uid) {
+			Some(Ordering::Equal) => Some(Ordering::Equal),
+			_ => Some(self.cmp(other)),
+		}
+	}
+}
+
+impl Ord for Employee {
+	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+		let self_value = self.experience / self.wage;
+		let other_value = other.experience / other.wage;
+		match self_value.cmp(&other_value) {
+			Ordering::Equal => Ordering::Equal,
+			Ordering::Less => Ordering::Less,
+			Ordering::Greater => Ordering::Greater,
+		}
+	}
+}
+
+// We want to parse employee information from a string data
+// The string data should be comma separated. Here are a few examples:
+//
+// * "Billy, 4, 5, 345" - Billy has been working here for 4 months and earns 5 token per hour. She
+//   is employee #345
+// * "Jose, 12, 6, 1" - Jose has been working here for 1 year (12 months) and earns 6
+// tokens per hour. He is employee #1
+//
+// Any strings with the wrong number of commas or numbers too big for `u32` should return `Err(_)`
+// where the error message may be anything.
+
+impl TryFrom<String> for Employee {
+	type Error = &'static str;
+
+	fn try_from(value: String) -> Result<Self, Self::Error> {
+		let parts: Vec<&str> = value.split(", ").collect();
+
+		if parts.len() != 4 {
+			return Err("Wrong value format");
+		}
+
+		let name = String::from(parts[0]);
+		let experience = parts[1].parse::<u32>().unwrap();
+		let wage = parts[2].parse::<u32>().unwrap();
+		let uid = parts[3].parse::<u32>().unwrap();
+
+		if experience == u32::MAX || wage == u32::MAX || uid == u32::MAX {
+			return Err("Invalid number format");
+		}
+
+		Ok(Employee {
+			name: name,
+			experience: experience,
+			wage: wage,
+			uid: uid,
+		})
+	}
+}
+
+// We also want to convert employees back into strings in the same format as above.
+impl From<Employee> for String {
+	fn from(e: Employee) -> Self {
+		let formated_string = format!("{}, {}, {}, {}", e.name, e.experience, e.wage, e.uid);
+		formated_string
+		// todo!()
+	}
+}
+
+/// This function is not graded. It is just for collecting feedback.
+/// On a scale from 0 - 255, with zero being extremely easy and 255 being extremely hard,
+/// how hard did you find this section of the exam.
+pub fn how_hard_was_this_section() -> u8 {
+	todo!()
+}
+
+/// This function is not graded. It is just for collecting feedback.
+/// How much time (in hours) did you spend on this section of the exam?
+pub fn how_many_hours_did_you_spend_on_this_section() -> u8 {
+	todo!()
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn employee_from_string_success() {
+		let s = String::from("Billy, 4, 5, 345");
+		let billy = Employee {
+			name: String::from("Billy"),
+			experience: 4,
+			wage: 5,
+			uid: 345,
+		};
+
+		assert_eq!(billy, s.try_into().unwrap())
+	}
+
+	#[test]
+	fn employee_to_string_success() {
+		let billy = Employee {
+			name: String::from("Billy"),
+			experience: 4,
+			wage: 5,
+			uid: 345,
+		};
+
+		assert_eq!(String::from("Billy, 4, 5, 345"), String::from(billy))
+	}
+
+	#[test]
+	fn employee_ord() {
+		let billy = Employee {
+			name: String::from("Billy"),
+			experience: 4,
+			wage: 5,
+			uid: 345,
+		};
+		let susie = Employee {
+			name: String::from("Susie"),
+			experience: 5,
+			wage: 5,
+			uid: 347,
+		};
+
+		assert!(susie > billy);
+	}
+
+	#[test]
+	fn employee_neq() {
+		let billy = Employee {
+			name: String::from("Billy"),
+			experience: 4,
+			wage: 5,
+			uid: 345,
+		};
+		let susie = Employee {
+			name: String::from("Susie"),
+			experience: 5,
+			wage: 5,
+			uid: 347,
+		};
+
+		assert!(susie != billy);
+	}
+}
